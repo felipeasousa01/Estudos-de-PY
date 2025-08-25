e01e3a93e73277f70dfa6558d1c5936fa4eeff28
felipeasousa01 <felipe.a.sousa01@gmail.com>
2025-08-25 16:42:34 -0300

    Create atividade loop Funcoes - Q07.py

---
+++ atividade loop Funcoes - Q07.py
@@ -0,0 +1,10 @@
+####### ATIVIDADE LOOP WHILE ######
+    # 07. Crie uma função que receba dois números e retorne a soma deles.
+
+def soma(num1, num2):
+    return num1 + num2  
+# Exemplo de uso da função
+numero1 = float(input("Digite o primeiro número: "))    
+numero2 = float(input("Digite o segundo número: "))
+resultado = soma(numero1, numero2)
+print("A soma dos números é:", resultado)
