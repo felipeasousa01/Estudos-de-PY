eb6813752fed617baf7ebe0cbb2c2600b5c2abc5
felipeasousa01 <felipe.a.sousa01@gmail.com>
2025-08-25 16:36:59 -0300

    Create atividade loop for - Q04.py

---
+++ atividade loop for - Q04.py
@@ -0,0 +1,6 @@
+####### ATIVIDADE LOOP WHILE ######
+    # 04. Faça um programa que mostre todos os números pares de 1 a 20.
+print("#### Números pares de 1 a 20 ####")
+for numero in range(1, 21):
+    if numero / 2 == 0:
+        print(numero)
